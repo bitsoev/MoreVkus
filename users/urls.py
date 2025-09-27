f05409d6510df4cb95b7d85d19f3e462162aab2f
bitsoev <168644301+bitsoev@users.noreply.github.com>
2025-09-27 18:17:49 +0300

    Доступ к регистрации, логаут, доступы к orders

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateUserView, CreateTokenView
+from .views import CreateUserView, CreateTokenView, ManageUserView, LogoutView
 
 urlpatterns = [
     path('register/', CreateUserView.as_view(), name='register'),
     path('login/', CreateTokenView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('me/', ManageUserView.as_view(), name='me'),
 ]
